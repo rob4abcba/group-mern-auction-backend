ed2c67fbbbc25222280f633881f3610602aa43a9
rob <rob_abcba@yahoo.com>
2023-10-22 17:00:58 -0700

    Add Model suffix to variable names to make things more clear for future contributors.  Remove EntriesModel since it is identical and redundant with AuctionItemsModel. POST new singular item, GET all plural items pattern applies for both AuctionItemsModel and BidsModel but not for UsersModel.

--- routes/allRoutes.js
+++ routes/allRoutes.js
@@ -4,11 +4,34 @@ const multer = require("multer");
 let getFields = multer();
 
 // module.exports = { AuctionItems, Users, Bids };
-const { AuctionItems, Users, Bids } = require("../models/allSchemas");
+// module.exports = { AuctionItemsModel, UsersModel, BidsModel, EntriesModel };
+module.exports = { AuctionItemsModel, UsersModel, BidsModel };
 
-//To get all the auctionItems information
+// const { AuctionItems, Users, Bids } = require("../models/allSchemas");
+const {
+  AuctionItemsModel,
+  UsersModel,
+  BidsModel,
+  // EntriesModel,
+} = require("../models/allSchemas");
+
+//To add a new auctionItem.
+allRouter.post("/additem", getFields.none(), async (request, response) => {
+  const newAuctionItem = new AuctionItemsModel(request.body);
+  let user = await newAuctionItem.save();
+  user = user.toObject();
+  // console.log(request);
+  try {
+    response.send(user);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+
+//To get all the existing auctionItems.
 allRouter.get("/", async (request, response) => {
-  const auctionItemsData = await AuctionItems.find({});
+  // const auctionItemsData = await AuctionItems.find({});
+  const auctionItemsData = await AuctionItemsModel.find({});
   try {
     response.send(auctionItemsData);
   } catch (error) {
@@ -16,12 +39,14 @@ allRouter.get("/", async (request, response) => {
   }
 });
 
-//To store the user data.
+//To add a new user.
 allRouter.post("/signup", getFields.none(), async (request, response) => {
-  const newuser = new Users(request.body);
-  let user = await newuser.save();
+  // const newUser = new Users(request.body);
+  const newUser = new UsersModel(request.body);
+
+  let user = await newUser.save();
   user = user.toObject();
-  // console.log(request);
+  console.log("request =", request);
   try {
     response.send(user);
   } catch (error) {
@@ -32,7 +57,8 @@ allRouter.post("/signup", getFields.none(), async (request, response) => {
 //To authenticate the user
 allRouter.post("/login", getFields.none(), async (request, response) => {
   console.log(request.body);
-  let user = await Users.findOne({
+  // let user = await Users.findOne({
+  let user = await UsersModel.findOne({
     email: request.body.email,
     password: request.body.password,
   });
@@ -45,9 +71,10 @@ allRouter.post("/login", getFields.none(), async (request, response) => {
   }
 });
 
-//To store the bid data.
-allRouter.post("/register", getFields.none(), async (request, response) => {
-  const newBid = new Bids(request.body);
+//To add a new bid.
+allRouter.post("/newbid", getFields.none(), async (request, response) => {
+  // const newBid = new Bids(request.body);
+  const newBid = new BidsModel(request.body);
   let bid = await newBid.save();
   bid = bid.toObject();
   try {
@@ -57,9 +84,10 @@ allRouter.post("/register", getFields.none(), async (request, response) => {
   }
 });
 
-//To get all the bid information
+//To get all the existing bids.
 allRouter.get("/allbids", async (request, response) => {
-  const bidData = await Bids.find({});
+  // const bidData = await Bids.find({});
+  const bidData = await BidsModel.find({});
   try {
     response.send(bidData);
   } catch (error) {
